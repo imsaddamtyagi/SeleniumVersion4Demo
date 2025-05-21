f48f803902c10a26477eb2880a0d0d8f8d9c8ac6
imsaddamtyagi <imsaddamtyagi@gmail.com>
2025-05-21 22:23:22 +0530

    ok

--- src/test/java/org/com/Collections/HashMapDemo.java
+++ src/test/java/org/com/Collections/HashMapDemo.java
@@ -1,55 +1,33 @@
 package org.com.Collections;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.LinkedHashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class HashMapDemo {
 	
-	
-	
-	public static void main(String[] args) {
-		
-		HashMap<String,Integer> MyHashMap = new HashMap<>();
-		
-		MyHashMap.put("Name",2);
-		MyHashMap.put("Khan",4);
-		MyHashMap.put("My",1);
-		MyHashMap.put("is",3);
-		
-		
-	  List<Map.Entry<String,Integer>> MyList = new ArrayList<>(MyHashMap.entrySet());
-	  
-	  
-	  MyList.sort((Entry1,Entry2) -> Entry1.getValue().compareTo(Entry2.getValue() ));
-	  
-	  
-	  LinkedHashMap<String,Integer> MyLinkedHasMap = new LinkedHashMap<>();
-	  
-	  
-	  
-	  for(Map.Entry<String, Integer> MyEntry: MyList)
-	  {
-		  
-		  MyLinkedHasMap.put(MyEntry.getKey(),MyEntry.getValue());
-		  
-	  }
-	  
-	  System.out.println("Printing sorted HashMap: ");
-	  for(Map.Entry<String, Integer> MyEntry: MyLinkedHasMap.entrySet())
-	  {
-		  
-	   System.out.println(MyEntry);
-		  
-	  }
-	  
-	  
-	  
-          
-		
-		
-	}
+    public static void main(String[] args) {
+    	
+        HashMap<String, Integer> MyHashMap = new HashMap<>();
+        MyHashMap.put("Name", 2);
+        MyHashMap.put("Khan", 4);
+        MyHashMap.put("My", 1);
+        MyHashMap.put("is", 3);
 
+
+        List<Map.Entry<String, Integer>> MyList = new ArrayList<>(MyHashMap.entrySet());
+
+
+        MyList.sort((Arg1, Arg2) -> Arg1.getValue().compareTo(Arg2.getValue()));
+
+  
+        LinkedHashMap<String, Integer> MyLinkedHashmap = new LinkedHashMap<>();
+        
+        for (Map.Entry<String, Integer> MyEntry : MyList) {
+            MyLinkedHashmap.put(MyEntry.getKey(), MyEntry.getValue());
+        }
+
+
+        for (Map.Entry<String, Integer> MyEntry : MyLinkedHashmap.entrySet()) {
+            System.out.println(MyEntry.getKey() + " -> " + MyEntry.getValue());
+        }
+    }
 }
--- src/test/java/org/com/Interview/ReverseString_PreserveSpace.java
+++ src/test/java/org/com/Interview/ReverseString_PreserveSpace.java
@@ -1,35 +1,15 @@
 package org.com.Interview;
 
 public class ReverseString_PreserveSpace {
+	
+	
+	
+	public static void  reverseString_PreserveSpaceTest(String str)
+	{
+		
+	}
 
  	
-    	static void reverseString_PreserveSpaceTest(String str)
-    	{
-    		                                                                                                                                                                                                                                                                                                                                                                                                                                                       
-    		char arr[] = str.toCharArray();
-    		
-    		char[] Result_Array = new char[str.length()];
-    		
-    		
-    		for(int i=0;i<arr.length;i++)
-    		{
-    		   if(arr[i]==' ')
-    		   {
-    			   Result_Array[i]=' ';
-    		   }
-    		}
-    		
-     		for(int i=0;i<arr.length;i++)
-     		{
-     			
-     		}
-    		
- 		
-    		
-    	}
-   
-    	
-  
      
     public static void main(String[] args) {
     	
